d219cfd8fd0b32887e496e555d1f03920d70db33
songhee <96781855+songhee1@users.noreply.github.com>
2023-08-31 08:43:40 +0900

    Add files via upload

---
+++ sp06_SpringMVC/src/main/java/com/service/spring/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.service.spring.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+public class IndexController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		return new ModelAndView("/WEB-INF/views/result.jsp", "hello", "Spring MVC~~");
+	}
+
+}
